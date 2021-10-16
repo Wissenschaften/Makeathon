fcfdb6cc2ba9a7db1bed58282be39a7132124a03
osmancalisir <osman.calisir@hotmail.com>
2021-10-16 18:50:42 +0300

    old gui removed

--- website/src/pages/foo/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-
-const Foo: NextPage = (props) => {
-    return (
-        <h3>Foo</h3>
-    );
-};
-
-export default Foo;
--- website/src/utils/safelySetInnerHtml.ts
+++
@@ -1,5 +0,0 @@
-const sanitizeHtml = require('sanitize-html');
-
-export const safelySetInnerHtml = (html: string) => {
-    return sanitizeHtml(html);
-};
