8e132c37a0e88dce276da8c1b4b632dda8ef3fda
osmancalisir <osman.calisir@hotmail.com>
2021-10-17 10:17:07 +0300

    infobox added

--- website/src/heatmap/map.js
+++ website/src/heatmap/map.js
@@ -17,21 +17,45 @@ class Map extends React.Component {
 
   render() {
     return (
-      <LeafletMap center={this.state.position} zoom={this.state.zoom}>
-        <HeatmapLayer
-          points={geojson}
-          longitudeExtractor={(m) => m[1]}
-          latitudeExtractor={(m) => m[0]}
-          intensityExtractor={(m) => parseFloat(m[2])}
-          max={100}
-          minOpacity={0.4}
-        />
+      <div class="heatmapContainer">
+        <div class="mapArea" style={{height: '85vh', width: '150vh'}}>
+          <LeafletMap center={this.state.position} zoom={this.state.zoom}>
+            <HeatmapLayer
+              points={geojson}
+              longitudeExtractor={(m) => m[1]}
+              latitudeExtractor={(m) => m[0]}
+              intensityExtractor={(m) => parseFloat(m[2])}
+              max={100}
+              minOpacity={0.4}
+            />
 
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-      </LeafletMap>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            />
+          </LeafletMap> 
+        </div>
+        <div class="infoArea">
+          <div class="starsArea" style={{width: '42vh'}}>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <p>Score: 2.6176367275604493</p>
+            <p>Expected driving time: 30min</p>
+            <p>Number of the taxis in the area: 38</p>
+          </div>
+          <div class="commentsArea">
+            
+          </div>
+          <div class="buttonArea">
+            <textarea id="w3review" name="w3review" rows="4" cols="36" placeholder="Your comments are valuable, plase tols us what you thought"></textarea>
+            <button class="button">Button</button>
+          </div>
+        </div>
+      </div>
+      
     );
   }
 }
